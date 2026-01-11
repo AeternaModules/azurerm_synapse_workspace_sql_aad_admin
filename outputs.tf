b0afd52a1d33114bc705c913a5c50e3543758168
Daniel Allen <dan@aeternagroup.net>
2026-01-11 19:38:24 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "synapse_workspace_sql_aad_admins" {
+  description = "All synapse_workspace_sql_aad_admin resources"
+  value       = azurerm_synapse_workspace_sql_aad_admin.synapse_workspace_sql_aad_admins
+}
+
